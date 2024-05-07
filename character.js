121273e1c10b9bd3ac1e6830c6b6e01e4e47bdf4
Jay Mistry <jaynyc1218@gmail.com>
2024-05-06 20:53:14 -0400

    Finished creating planets v1

--- character.js
+++ character.js
@@ -58,7 +58,24 @@ const renderCharacter = character => {
   heightSpan.textContent = character?.height;
   massSpan.textContent = character?.mass;
   birthYearSpan.textContent = character?.birth_year;
-  homeworldSpan.innerHTML = `<a href="/planet.html?id=${character?.homeworld.id}">${character?.homeworld.name}</a>`;
+  homeworldSpan.innerHTML = `<a href="/planets.html?id=${character?.homeworld.id}">${character?.homeworld.name}</a>`;
+  
+  homeworldSpan.addEventListener('click', function() {
+    goToPlanetPage(planet.id)
+  });
+  
   const filmsLis = character?.films?.map(film => `<li><a href="/film.html?id=${film.id}">${film.title}</li>`)
   filmsUl.innerHTML = filmsLis.join("");
+
+
 }
+
+/*
+function getPlanet() {
+  document.getElementById('planets').addEventListener('click', function() {
+    goToPlanetPage(planet.id)
+  });
+}
+*/
+
+const goToPlanetPage = id => window.location = `/planets.html?id=${id}`
---
+++ planets.js
@@ -0,0 +1,85 @@
+let nameH1;
+let climateSpan;
+let surfaceWaterSpan;
+let diameterSpan;
+let rotationPeriodSpan;
+let terrainSpan;
+let gravitySpan;
+let orbitalPeriodSpan;
+let populationSpan;
+let filmsDiv;
+let residentsDiv;
+const baseUrl = `https://swapi2.azurewebsites.net/api`;
+
+// Runs on page load
+addEventListener('DOMContentLoaded', () => {
+  nameH1 = document.querySelector('h1#name');
+  climateSpan = document.querySelector('span#climate');
+  surfaceWaterSpan = document.querySelector('span#surface_water');
+  diameterSpan = document.querySelector('span#diameter');
+  rotationPeriodSpan = document.querySelector('span#rotation_period');
+  terrainSpan = document.querySelector('span#terrain');
+  gravitySpan = document.querySelector('span#gravity');
+  orbitalPeriodSpan = document.querySelector('span#orbital_period');
+  populationSpan = document.querySelector('span#population');
+
+  characterUl = document.querySelector('#characters>ul')
+  filmsUl = document.querySelector('#films>ul');
+  const sp = new URLSearchParams(window.location.search)
+  const id = sp.get('id')
+  getPlanet(id)
+});
+
+async function getPlanet(id) {
+  let planet;
+  try {
+    planet = await fetchPlanet(id)
+    planet.characters = await fetchCharacter(planet)
+    planet.films = await fetchFilms(planet)
+  }
+  catch (ex) {
+    console.error(`Error reading Planet ${id} data.`, ex.message);
+  }
+  renderPlanet(planet);
+
+}
+async function fetchPlanet(id) {
+  let planetUrl = `${baseUrl}/planets/${id}`;
+  return await fetch(planetUrl)
+    .then(res => res.json())
+}
+
+async function fetchCharacter(planet) {
+  const url = `${baseUrl}/planets/${planet?.id}/characters`;
+  const characters = await fetch(url)
+    .then(res => res.json())
+  return characters;
+}
+
+async function fetchFilms(planet) {
+  const url = `${baseUrl}/planets/${planet?.id}/films`;
+  const films = await fetch(url)
+    .then(res => res.json())
+  return films;
+}
+
+const renderPlanet = planet => {
+  document.title = `SWAPI - ${planet?.name}`;  // Just to make the browser tab say their name
+  nameH1.textContent = planet?.name;
+  climateSpan.textContent = planet?.climate;
+  surfaceWaterSpan.textContent = planet?.surface_water;
+  diameterSpan.textContent = planet?.diameter;
+  rotationPeriodSpan.textContent = planet?.rotation_period;
+  terrainSpan.textContent = planet?.terrain;
+  gravitySpan.textContent = planet?.gravity;
+  orbitalPeriodSpan.textContent = planet?.orbital_period;
+  populationSpan.textContent = planet?.population;
+
+  //homeworldSpan.innerHTML = `<a href="/planet.html?id=${character?.homeworld.id}">${character?.homeworld.name}</a>`;
+
+  const characterLis = planet?.characters?.map(character => `<li><a href="/character.html?id=${character.id}">${character.name}</li>`)
+  characterUl.innerHTML = characterLis.join("");
+
+  const filmsLis = planet?.films?.map(film => `<li><a href="/film.html?id=${film.id}">${film.title}</li>`)
+  filmsUl.innerHTML = filmsLis.join("");
+}
